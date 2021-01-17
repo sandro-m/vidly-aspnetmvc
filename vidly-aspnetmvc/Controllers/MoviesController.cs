bf0701f1c4d39d68fceb17b2e2e0d20dc53d13b5
sandro-m <sandrohmorita@gmail.com>
2021-01-17 16:19:21 -0300

    MVC em Ação

---
+++ vidly-aspnetmvc/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly_aspnetmvc.Models;
+
+namespace vidly_aspnetmvc.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Id = 1,
+                Name = "Shrek"
+            };
+
+            return View(movie);
+        }
+    }
+}
